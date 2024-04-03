d0d6ce2487e4d1307b04ec41efad7b703f720d98
Ntsako Ngwana <microdarkcosm@outlook.com>
2024-04-04 01:03:00 +0200

    Stable 3

--- p24/p24/spiders/P24.py
+++ p24/p24/spiders/P24.py
@@ -2,7 +2,9 @@ import scrapy
 import re
 import time
 import random
-from bs4 import BeautifulSoup
+import asyncio
+from scrapy.http import TextResponse
+import requests
 
 def extract_number(text):
     cleaned = text.replace("\\r\\n",'').strip()
@@ -22,7 +24,6 @@ def keep_link(string):
 
   if match:
       link = match.group(1)
-      #print("Extracted link:", link)
       return link
 
   
@@ -30,15 +31,29 @@ class PropScrapper(scrapy.Spider):
 
   name = "propscrap"
   allowed_domains = ["www.property24.com/"]
-  start_urls = ['https://www.property24.com/houses-for-sale/gauteng/1/p2?sp=eszf%3d100%26bd%3d1%26bth%3d1%26ps%3d1' ]
+  start_urls = ['https://www.property24.com/houses-for-sale/gauteng/1?sp=eszf%3d100%26bd%3d1%26bth%3d1%26ps%3d1' ]
   #indices = [f"https://www.property24.com/houses-for-sale/gauteng/1/p{pno}?sp=eszf%3d100%26bd%3d1%26bth%3d1%26ps%3d1" for pno in range(2,10)]
 
   #for i in indices :
    # start_urls.append(i)
  
   prices_dump =[]
-  def parsecontent(self,response):
-    description = response.css(".js_readMoreContainer::text").extract()
+  async def parsecontent(self,link):
+    time.sleep(random.randint(1, 13)) 
+    resp = requests.get(link)
+    resp = TextResponse(body=resp.content, url=link)
+
+    # Using XPath to target the specific div containing the text data
+    text_div = resp.xpath('//div[@class="js_readMoreText p24_readMoreText"]')
+
+    # Extracting text from all child p tags within the targeted div
+    text_data = text_div.xpath('.//p/text()').getall()
+
+    # Joining the extracted text data into a single string
+    full_text = ' '.join(text_data)
+    time.sleep(random.randint(1, 3)) 
+
+    return f"---------------{full_text}****-------------------------------------"
 
   def parse(self, response):
     
@@ -49,6 +64,7 @@ class PropScrapper(scrapy.Spider):
     anchor_selector2 = response.xpath('//a[@class="p24_regularTile js_resultTileClickable js_rollover_container "]')
     hrefs2 = [f"https://www.property24.com{ref.xpath('@href').get()}" for ref in anchor_selector2]
 
+
     bedrooms = response.css(".p24_featureDetails:nth-child(1)").extract()
     bathroom = response.css(".p24_featureDetails:nth-child(2)").extract()
     parking = response.css(".p24_featureDetails~ .p24_featureDetails+ .p24_featureDetails").extract()
@@ -58,15 +74,22 @@ class PropScrapper(scrapy.Spider):
     price = [extract_number(p) for p in price if extract_number(p) != None]
    
     #Cleaned Data
-   
+    links = hrefs1 + hrefs2
     bedrooms  = [extract_number(remove_tags(b)) for b in bedrooms]
     bathrooms = [extract_number(remove_tags(b)) for b in bathroom]
     parkings =[extract_number(remove_tags(b)) for b in parking]
     sizes = [extract_number(remove_tags(b)) for b in size]
     locations = [remove_tags(loc) for loc in location]
-    links = hrefs1 + hrefs2
-   
+    descriptions = []
+
+    for link in links:
+      loop = asyncio.get_event_loop()
+      description = loop.run_until_complete(self.parsecontent(link))
+       
+      descriptions.append(description)
+      
     time.sleep(random.randint(5, 10)) 
+
 
   
     
@@ -74,7 +97,7 @@ class PropScrapper(scrapy.Spider):
     yield {"price":price,"Content count": len(price),"bedrooms":bedrooms,
     "bed":len(bedrooms),"loc":locations,"loclen":len(locations),"bathrooms":bathrooms,
     "bathroomlen":len(bathrooms),"size":sizes,"sizelen":len(sizes),"parking":parkings,"plen":len(parkings),
-    "links":  links, "linkslen": len(links)
+    "links":  links, "linkslen": len(links),"descriptions": descriptions,"descriptionslen":len(descriptions)
 
 
     }
